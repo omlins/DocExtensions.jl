8e8795585d2dabb4318845886566835ef9192bb5
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 18:15:16 +0200

    add DocumenterExtensions module

---
+++ src/DocumenterExtensions/DocumenterExtensions.jl
@@ -0,0 +1,17 @@
+"""
+Module DocumenterExtensions
+
+Provides extensions to Documenter.jl.
+"""
+module DocumenterExtensions
+
+## Alphabetical include of submodules.
+include(joinpath("..", "Exceptions.jl"))
+using .Exceptions
+
+## Alphabetical include of function/data type definition files
+
+
+## Exports (need to be after include of submodules if re-exports from them)
+
+end
