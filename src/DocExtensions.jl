6a35819abe5962363ff1b224288bb6053c3a25f9
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 18:15:05 +0200

    add DocumenterExtensions module

--- src/DocExtensions.jl
+++ src/DocExtensions.jl
@@ -1,19 +1,22 @@
 """
 Module DocExtensions
 
-Provides extensions to documentation tools as Documenter.jl and Literate.jl.
+Provides extensions to documentation tools as Documenter.jl and Literate.jl, including a simple file processor for pre- or post-processing of files.
 """
 module DocExtensions
 
 ## Alphabetical include of submodules.
 include("Exceptions.jl")
 include(joinpath("FileProcessor","FileProcessor.jl"))
+include(joinpath("DocumenterExtensions","DocumenterExtensions.jl"))
 using .Exceptions
 import .FileProcessor
+import .DocumenterExtensions
 
 ## Alphabetical include of function/data type definition files
 include("expand_reflinks.jl")
 
 ## Exports (need to be after include of submodules if re-exports from them)
-export FileProcessor, expand_reflinks
+export FileProcessor, DocumenterExtensions
+export expand_reflinks
 end
