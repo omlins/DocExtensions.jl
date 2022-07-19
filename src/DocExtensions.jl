ac67e3f48f3faebe0882f36ad9adb42b6e800aaa
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 17:55:00 +0200

    add main module

--- src/DocExtensions.jl
+++ src/DocExtensions.jl
@@ -1,5 +1,19 @@
+"""
+Module DocExtensions
+
+Provides extensions to documentation tools as Documenter.jl and Literate.jl.
+"""
 module DocExtensions
 
-# Write your package code here.
+## Alphabetical include of submodules.
+include("Exceptions.jl")
+include(joinpath("FileProcessor","FileProcessor.jl"))
+using .Exceptions
+import .FileProcessor
 
+## Alphabetical include of function/data type definition files
+include("expand_reflinks.jl")
+
+## Exports (need to be after include of submodules if re-exports from them)
+export FileProcessor, expand_reflinks
 end
