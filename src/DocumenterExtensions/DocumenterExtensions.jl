00006e6ca5afc81a0acb00b16cddb9cb1c0af1fd
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 19:14:40 +0200

    add mdinclude

--- src/DocumenterExtensions/DocumenterExtensions.jl
+++ src/DocumenterExtensions/DocumenterExtensions.jl
@@ -10,8 +10,8 @@ include(joinpath("..", "Exceptions.jl"))
 using .Exceptions
 
 ## Alphabetical include of function/data type definition files
-
+include("mdinclude.jl")
 
 ## Exports (need to be after include of submodules if re-exports from them)
-
+export mdinclude
 end
