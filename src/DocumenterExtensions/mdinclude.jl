65f30488c0fc580a39edb0badc46280e31cb9a64
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 19:19:59 +0200

    add mdinclude docstring

--- src/DocumenterExtensions/mdinclude.jl
+++ src/DocumenterExtensions/mdinclude.jl
@@ -1,7 +1,9 @@
 using Markdown
 
 """
+    mdinclude(path)
 
+Include a file as markdown content. Files with a ".jl"-extension are included as a Julia code block; files with any other extension are parsed as markdown. 
 """
 function mdinclude(path::AbstractString)
     if endswith(lowercase(path), ".jl")
