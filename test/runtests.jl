047a96fce672e83c5fea98ddeac45b530d82c59e
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 12:31:48 +0200

    add runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using DocExtensions
-using Test
+push!(LOAD_PATH, "../src")
 
-@testset "DocExtensions.jl" begin
-    # Write your tests here.
+import DocExtensions # Precompile it.
+
+excludedfiles = [ "test_excluded.jl"];
+
+function runtests()
+    exename   = Base.julia_cmd()
+    testdir   = pwd()
+    istest(f) = endswith(f, ".jl") && startswith(f, "test_")
+    testfiles = sort(filter(istest, readdir(testdir)))
+
+    nfail = 0
+    printstyled("Testing package DocExtensions.jl\n"; bold=true, color=:white)
+    for f in testfiles
+        println("")
+        if f ∈ excludedfiles
+            println("Test Skip:")
+            println("$f")
+            continue
+        end
+        try
+            run(`$exename -O3 --startup-file=no $(joinpath(testdir, f))`)
+        catch ex
+            nfail += 1
+        end
+    end
+    return nfail
 end
+exit(runtests())
